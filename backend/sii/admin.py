d604062ef273f7a5574970a3c60bf28791dc9b89
Joaquin <puig.joaco@gmail.com>
2026-05-27 07:33:16 -0400

    Redact SII admin refs

--- backend/sii/admin.py
+++ backend/sii/admin.py
@@ -1,18 +1,278 @@
 from django.contrib import admin
 
-from .models import CapacidadTributariaSII, DTEEmitido
+from core.reference_validation import redact_sensitive_payload, redact_sensitive_reference
+
+from .models import (
+    CapacidadTributariaSII,
+    DDJJPreparacionAnual,
+    DTEEmitido,
+    F22PreparacionAnual,
+    F29PreparacionMensual,
+    ProcesoRentaAnual,
+)
+
+
+def _redacted_attr(obj, field_name):
+    return redact_sensitive_reference(getattr(obj, field_name, '')) or ''
+
+
+def _redacted_payload_attr(obj, field_name):
+    return redact_sensitive_payload(getattr(obj, field_name, None))
 
 
 @admin.register(CapacidadTributariaSII)
 class CapacidadTributariaSIIAdmin(admin.ModelAdmin):
-    list_display = ('empresa', 'capacidad_key', 'ambiente', 'estado_gate', 'evidencia_ref')
+    fields = (
+        'empresa',
+        'capacidad_key',
+        'certificado_ref_redacted',
+        'evidencia_ref_redacted',
+        'prueba_flujo_ref_redacted',
+        'autorizacion_ambiente_ref_redacted',
+        'regla_fiscal_ref_redacted',
+        'ambiente',
+        'estado_gate',
+        'ultimo_resultado_redacted',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = (
+        'certificado_ref_redacted',
+        'evidencia_ref_redacted',
+        'prueba_flujo_ref_redacted',
+        'autorizacion_ambiente_ref_redacted',
+        'regla_fiscal_ref_redacted',
+        'ultimo_resultado_redacted',
+        'created_at',
+        'updated_at',
+    )
+    list_display = ('empresa', 'capacidad_key', 'ambiente', 'estado_gate', 'evidencia_ref_redacted')
     list_filter = ('capacidad_key', 'ambiente', 'estado_gate')
-    search_fields = ('empresa__razon_social', 'certificado_ref', 'evidencia_ref', 'prueba_flujo_ref', 'regla_fiscal_ref')
+    search_fields = ('empresa__razon_social',)
+
+    @admin.display(description='certificado_ref')
+    def certificado_ref_redacted(self, obj):
+        return _redacted_attr(obj, 'certificado_ref')
+
+    @admin.display(description='evidencia_ref')
+    def evidencia_ref_redacted(self, obj):
+        return _redacted_attr(obj, 'evidencia_ref')
+
+    @admin.display(description='prueba_flujo_ref')
+    def prueba_flujo_ref_redacted(self, obj):
+        return _redacted_attr(obj, 'prueba_flujo_ref')
+
+    @admin.display(description='autorizacion_ambiente_ref')
+    def autorizacion_ambiente_ref_redacted(self, obj):
+        return _redacted_attr(obj, 'autorizacion_ambiente_ref')
+
+    @admin.display(description='regla_fiscal_ref')
+    def regla_fiscal_ref_redacted(self, obj):
+        return _redacted_attr(obj, 'regla_fiscal_ref')
+
+    @admin.display(description='ultimo_resultado')
+    def ultimo_resultado_redacted(self, obj):
+        return _redacted_payload_attr(obj, 'ultimo_resultado')
+
+    def has_add_permission(self, request):
+        return False
 
 
 @admin.register(DTEEmitido)
 class DTEEmitidoAdmin(admin.ModelAdmin):
+    fields = (
+        'empresa',
+        'capacidad_tributaria',
+        'contrato',
+        'pago_mensual',
+        'distribucion_cobro_mensual',
+        'arrendatario',
+        'tipo_dte',
+        'monto_neto_clp',
+        'fecha_emision',
+        'estado_dte',
+        'sii_track_id_redacted',
+        'ultimo_estado_sii',
+        'observaciones_redacted',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = (
+        'sii_track_id_redacted',
+        'observaciones_redacted',
+        'created_at',
+        'updated_at',
+    )
     list_display = ('empresa', 'tipo_dte', 'pago_mensual', 'monto_neto_clp', 'estado_dte', 'fecha_emision')
     list_filter = ('tipo_dte', 'estado_dte')
-    search_fields = ('empresa__razon_social', 'sii_track_id', 'ultimo_estado_sii')
+    search_fields = ('empresa__razon_social', 'ultimo_estado_sii')
 
+    @admin.display(description='sii_track_id')
+    def sii_track_id_redacted(self, obj):
+        return _redacted_attr(obj, 'sii_track_id')
+
+    @admin.display(description='observaciones')
+    def observaciones_redacted(self, obj):
+        return _redacted_attr(obj, 'observaciones')
+
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(F29PreparacionMensual)
+class F29PreparacionMensualAdmin(admin.ModelAdmin):
+    fields = (
+        'empresa',
+        'capacidad_tributaria',
+        'cierre_mensual',
+        'anio',
+        'mes',
+        'estado_preparacion',
+        'resumen_formulario_redacted',
+        'borrador_ref_redacted',
+        'observaciones_redacted',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = (
+        'resumen_formulario_redacted',
+        'borrador_ref_redacted',
+        'observaciones_redacted',
+        'created_at',
+        'updated_at',
+    )
+    list_display = ('empresa', 'anio', 'mes', 'estado_preparacion', 'borrador_ref_redacted')
+    list_filter = ('estado_preparacion', 'anio', 'mes')
+    search_fields = ('empresa__razon_social',)
+
+    @admin.display(description='resumen_formulario')
+    def resumen_formulario_redacted(self, obj):
+        return _redacted_payload_attr(obj, 'resumen_formulario')
+
+    @admin.display(description='borrador_ref')
+    def borrador_ref_redacted(self, obj):
+        return _redacted_attr(obj, 'borrador_ref')
+
+    @admin.display(description='observaciones')
+    def observaciones_redacted(self, obj):
+        return _redacted_attr(obj, 'observaciones')
+
+    def has_add_permission(self, request):
+        return False
+
+
+class AnnualTaxArtifactAdminMixin(admin.ModelAdmin):
+    list_filter = ('estado_preparacion', 'anio_tributario')
+    search_fields = ('empresa__razon_social',)
+
+    @admin.display(description='observaciones')
+    def observaciones_redacted(self, obj):
+        return _redacted_attr(obj, 'observaciones')
+
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(ProcesoRentaAnual)
+class ProcesoRentaAnualAdmin(admin.ModelAdmin):
+    fields = (
+        'empresa',
+        'anio_tributario',
+        'estado',
+        'fecha_preparacion',
+        'resumen_anual_redacted',
+        'paquete_ddjj_ref_redacted',
+        'borrador_f22_ref_redacted',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = (
+        'fecha_preparacion',
+        'resumen_anual_redacted',
+        'paquete_ddjj_ref_redacted',
+        'borrador_f22_ref_redacted',
+        'created_at',
+        'updated_at',
+    )
+    list_display = ('empresa', 'anio_tributario', 'estado', 'paquete_ddjj_ref_redacted', 'borrador_f22_ref_redacted')
+    list_filter = ('estado', 'anio_tributario')
+    search_fields = ('empresa__razon_social',)
+
+    @admin.display(description='resumen_anual')
+    def resumen_anual_redacted(self, obj):
+        return _redacted_payload_attr(obj, 'resumen_anual')
+
+    @admin.display(description='paquete_ddjj_ref')
+    def paquete_ddjj_ref_redacted(self, obj):
+        return _redacted_attr(obj, 'paquete_ddjj_ref')
+
+    @admin.display(description='borrador_f22_ref')
+    def borrador_f22_ref_redacted(self, obj):
+        return _redacted_attr(obj, 'borrador_f22_ref')
+
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(DDJJPreparacionAnual)
+class DDJJPreparacionAnualAdmin(AnnualTaxArtifactAdminMixin):
+    fields = (
+        'empresa',
+        'capacidad_tributaria',
+        'proceso_renta_anual',
+        'anio_tributario',
+        'estado_preparacion',
+        'resumen_paquete_redacted',
+        'paquete_ref_redacted',
+        'observaciones_redacted',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = (
+        'resumen_paquete_redacted',
+        'paquete_ref_redacted',
+        'observaciones_redacted',
+        'created_at',
+        'updated_at',
+    )
+    list_display = ('empresa', 'anio_tributario', 'estado_preparacion', 'paquete_ref_redacted')
+
+    @admin.display(description='resumen_paquete')
+    def resumen_paquete_redacted(self, obj):
+        return _redacted_payload_attr(obj, 'resumen_paquete')
+
+    @admin.display(description='paquete_ref')
+    def paquete_ref_redacted(self, obj):
+        return _redacted_attr(obj, 'paquete_ref')
+
+
+@admin.register(F22PreparacionAnual)
+class F22PreparacionAnualAdmin(AnnualTaxArtifactAdminMixin):
+    fields = (
+        'empresa',
+        'capacidad_tributaria',
+        'proceso_renta_anual',
+        'anio_tributario',
+        'estado_preparacion',
+        'resumen_f22_redacted',
+        'borrador_ref_redacted',
+        'observaciones_redacted',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = (
+        'resumen_f22_redacted',
+        'borrador_ref_redacted',
+        'observaciones_redacted',
+        'created_at',
+        'updated_at',
+    )
+    list_display = ('empresa', 'anio_tributario', 'estado_preparacion', 'borrador_ref_redacted')
+
+    @admin.display(description='resumen_f22')
+    def resumen_f22_redacted(self, obj):
+        return _redacted_payload_attr(obj, 'resumen_f22')
+
+    @admin.display(description='borrador_ref')
+    def borrador_ref_redacted(self, obj):
+        return _redacted_attr(obj, 'borrador_ref')
